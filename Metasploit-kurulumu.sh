83414faeeccc8106b0367e0d43937c6b3ee78d54
hacker511551 <38014045+hacker511551@users.noreply.github.com>
2021-10-03 00:20:41 +0300

    Update Metasploit-kurulumu.sh

--- Metasploit-kurulumu.sh
+++ Metasploit-kurulumu.sh
@@ -1,13 +1,9 @@
-prinf "yüklemeyi takip ediniz"
 cd
 cd
 pkg update && pkg upgrade -y
-pkg install wget -y
-pkg install git -y
-wget https://github.com/Hax4us/TermuxBlack/raw/master/install.sh
-bash install.sh -i
-wget https://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh
-cd Metasploit_termux
-bash metasploit.sh
-rm -f $PREFIX/etc/apt/sources.list.d/termuxblack.list && mv $PREFIX/etc/bash.bashrc.bk $PREFIX/etc/bash.bashrc && rm -f ~/.termux/colors.properties
-prinf "termuxu yeniden başlat"
+Pkg install postgresql openssh wget git -y
+wget https://raw.githubusercontent.com/gushmazuko/metasploit_in_termux/master/metasploit.sh
+chmod +x metasploit.sh
+./metasploit.sh
+cd
+cd
